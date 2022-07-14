33cc93c4449af248eba4995e498d41df04a67c00
olaleye <olaoduola01@gmail.com>
2022-07-14 12:45:28 +0100

    java if else

---
+++ first.java
@@ -0,0 +1,15 @@
+public class first {
+    public static void main(String[] args) {
+        int myNum = 40;
+        if (myNum >= 40) {
+            System.out.println("you are last");
+        }
+        else if(myNum == 30) {
+            System.out.println("you are THE same");
+        
+
+        }
+
+      
+    }
+  }
