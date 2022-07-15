77e08c85baaf400eb20f45ea48ba1db4378ee5f2
olaleye <olaoduola01@gmail.com>
2022-07-15 10:07:13 +0100

    exercise.java added

---
+++ exercise.java
@@ -0,0 +1,22 @@
+public class exercise {
+    public static void main(String[] args) {
+        String name = "Olaleye";
+        String knowName = "Leye";
+        int age = 21;
+
+        System.out.println("My name is " + name);
+        System.out.println("Known as " + knowName);
+        System.out.println("My age is " + age);
+
+        int m = 1;
+        double x = 0.5;
+        int c = 15;
+        double y;
+        y = (m * x + c);
+        System.out.println("Y is equal to " + y);
+
+        
+
+        
+    }
+}
