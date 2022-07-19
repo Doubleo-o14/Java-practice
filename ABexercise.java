007c29f7cbe2def40cf31b84f7e8d729f417a3e4
olaleye <olaoduola01@gmail.com>
2022-07-19 13:34:50 +0100

    what method do i prefer

--- ABexercise.java
+++ ABexercise.java
@@ -22,11 +22,12 @@ class ABexercise {
         //     System.out.println(i + 10);
 
         // }
-        int stored_i = 1;
-        for (int i =1; i<10; i++) {
-        System.out.println(i + " 2^1: " +  (stored_i *= 2));
+    //     int stored_i = 1;
+    //     for (int i =1; i<10; i++) {
+    //     System.out.println(i + " 2^1: " +  (stored_i *= 2));
     
-    }
+    // }
+    System.out.println("I prefer using the for loop as the code looks more cleaner.");
 
 
 
