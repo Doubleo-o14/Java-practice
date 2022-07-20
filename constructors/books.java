2061850f1315e9588ef8a75ea66ca30bd9dc7643
olaleye <olaoduola01@gmail.com>
2022-07-20 04:12:41 +0100

    constructor is set

--- constructors/books.java
+++ constructors/books.java
@@ -4,4 +4,11 @@ public class books {
     String author;
     String name;
     int price;
+
+    public books(String bookAuthor, String bookName, int bookPrice){
+        bookAuthor = author;
+        bookName = name;
+        bookPrice = price;
+
+    }
 }
